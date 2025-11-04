a83b2cbb695cb83861a0073b95ecc6240ef7e0f6
Thecarisma <iamthecarisma@gmail.com>
2025-11-04 10:23:20 +0100

    use ncc for compilation - test 1

--- index.ts
+++ index.ts
@@ -38,7 +38,7 @@ async function prepareEnvironmentVars() {
             acc[environmentVarsWritePrefix + k] = environmentVars[k];
             return acc;
         }, {});
-        console.log("PROC=", core.getInput("UAT_SSH_USERNAME"));
+        console.log("PROC=", await invokeGithubApi());
         console.log("ENV=", environment);
         console.log("ENV-CASING=", environmentCasing);
         console.log("ENV-VARS-READ-PREFIX - (PRE)=", environmentVarsReadPrefixRaw);
@@ -81,6 +81,23 @@ function expandVariables(value: string) {
         result += c;
     }
     return result;
+}
+
+async function invokeGithubApi() {
+    const githubToken = process.env.GITHUB_TOKEN;
+    if (!githubToken) {
+        core.setFailed("GITHUB_TOKEN is not set in the environment.");
+        return;
+    }
+    const octokit = github.getOctokit(githubToken);
+    const { owner, repo } = github.context.repo;
+    const response = await octokit.rest.actions.listRepoVariables({
+        owner,
+        repo,
+    });
+
+    console.log("WE FOUND ---", response);
+    core.info(`Found ${response}`);
 }
 
 function getInput(name: string, type: string = "string", defaultValue?: any) {
