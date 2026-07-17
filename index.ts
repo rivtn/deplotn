0c9785d0882c09dc611349596aa88e008821dc54
Thecarisma <iamthecarisma@gmail.com>
2026-07-17 18:17:16 +0100

    remove default rivtn doicker host

--- index.ts
+++ index.ts
@@ -93,7 +93,7 @@ async function prepareEnvironmentVars() {
 }
 
 async function buildAndPushDockerImage(onComplete: () => void) {
-    if (!getInput("dockerize", "boolean")) {
+    if (true) {
         onComplete();
         return;
     }
